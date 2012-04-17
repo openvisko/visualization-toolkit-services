c8ddd2b5b59bc82d80c3a844fef0872baed8abcc
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-17 11:12:50 -0600

    changed project name to just services

---
+++ services/src/edu/utep/trustlab/services/NCLServices.java
@@ -0,0 +1,80 @@
+package edu.utep.trustlab.services;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import edu.utep.trustlab.toolkitOperators.ncl.ESRIGriddedToContourMapPS;
+import edu.utep.trustlab.toolkitOperators.ncl.ESRIGriddedToRasterMapPS;
+import edu.utep.trustlab.toolkitOperators.ncl.NetCDFToContourMapPS;
+import edu.utep.trustlab.toolkitOperators.ncl.NetCDFToRasterMapPS;
+import edu.utep.trustlab.toolkitOperators.ncl.NetCDFToTimeSeriesPlotPS;
+
+
+
+@WebService(targetNamespace="http://nclTrustLab/", serviceName="NCLTransformers")
+public class NCLServices
+{
+	public String esriGridContour(
+			@WebParam(name="url") String url,
+			@WebParam(name="font") String font,
+			@WebParam(name="lbOrientation") String lbOrientation,
+			@WebParam(name="cnLevelSpacingF") String cnLevelSpacingF,
+			@WebParam(name="colorTable") String colorTable,
+			@WebParam(name="cnLinesOn") String cnLinesOn,
+			@WebParam(name="cnFillOn") String cnFillOn
+	)
+	{
+		ESRIGriddedToContourMapPS service = new ESRIGriddedToContourMapPS(url);
+		return service.transform(font, lbOrientation, cnLevelSpacingF, colorTable, cnLinesOn, cnFillOn);
+	}
+	
+	public String esriGridRaster(
+			@WebParam(name="url") String url,
+			@WebParam(name="font") String font,
+			@WebParam(name="lbOrientation") String lbOrientation,
+			@WebParam(name="colorTable") String colorTable)
+	{
+		ESRIGriddedToRasterMapPS service = new ESRIGriddedToRasterMapPS(url);
+		return service.transform(font, lbOrientation, colorTable);
+	}
+	
+	public String netCDFGridContour(
+			@WebParam(name="url") String url,
+			@WebParam(name="plotVariable") String plotVariable,
+			@WebParam(name="font") String font,
+			@WebParam(name="lbOrientation") String lbOrientation, 
+			@WebParam(name="cnLevelSpacingF") String cnLevelSpacingF,
+			@WebParam(name="colorTable") String colorTable,
+			@WebParam(name="cnLinesOn") String cnLinesOn,
+			@WebParam(name="cnFillOn") String cnFillOn
+	)
+	{
+		NetCDFToContourMapPS service = new NetCDFToContourMapPS(url);
+		return service.transform(plotVariable, font, lbOrientation, cnLevelSpacingF, colorTable, cnLinesOn, cnFillOn);
+	}
+	
+	public String netCDFGridRaster(
+			@WebParam(name="url") String url,
+			@WebParam(name="plotVariable") String plotVariable,
+			@WebParam(name="font") String font,
+			@WebParam(name="lbOrientation") String lbOrientation, 
+			@WebParam(name="colorTable") String colorTable
+	)
+	{
+		NetCDFToRasterMapPS service = new NetCDFToRasterMapPS(url);
+		return service.transform(plotVariable, font, lbOrientation, colorTable);
+	}
+	
+	public String netCDFTimeSeries(
+			@WebParam(name="url") String url,
+			@WebParam(name="lPlotVariablesList") String lPlotVariablesList,
+			@WebParam(name="rPlotVariablesList") String rPlotVariablesList,
+			@WebParam(name="xDimName") String xDimName,
+			@WebParam(name="xDimSize") String xDimSize,
+			@WebParam(name="title") String title,
+			@WebParam(name="yLAxisLabel") String yLAxisLabel,
+			@WebParam(name="yRAxisLabel") String yRAxisLabel)
+	{
+		NetCDFToTimeSeriesPlotPS service = new NetCDFToTimeSeriesPlotPS(url);
+		return service.transform(lPlotVariablesList, rPlotVariablesList, xDimName, xDimSize, title, yLAxisLabel, yRAxisLabel);
+	}
+}
