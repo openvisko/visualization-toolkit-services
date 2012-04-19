a4d605bb53a83c826cff41120e9290df8b98a75d
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-18 18:26:04 -0600

    added package info to set qualified wsdl

---
+++ toolkits/src/edu/utep/trustlab/services/package-info.java
@@ -0,0 +1,5 @@
+@javax.xml.bind.annotation.XmlSchema (
+		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+		attributeFormDefault = javax.xml.bind.annotation.XmlNsForm.UNQUALIFIED)
+
+package edu.utep.trustlab.services;
