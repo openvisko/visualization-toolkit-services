fbe39a55c08434085c582d8c326ad4929f5f5bc1
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-19 12:30:48 -0600

    setting schema to not rely on namespaces

--- toolkits/src/edu/utep/trustlab/services/package-info.java
+++ toolkits/src/edu/utep/trustlab/services/package-info.java
@@ -1,5 +1,5 @@
 @javax.xml.bind.annotation.XmlSchema (
 		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
-		attributeFormDefault = javax.xml.bind.annotation.XmlNsForm.UNQUALIFIED)
+		attributeFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 
 package edu.utep.trustlab.services;
