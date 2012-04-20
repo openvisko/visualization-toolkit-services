f050eb5249e62eae53dc142147757b2bb01d6d54
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-19 23:38:28 -0600

    changed back to axis2 cause i got that working over cxf :(

--- toolkits/src/edu/utep/trustlab/services/context/ContextListener.java
+++ toolkits/src/edu/utep/trustlab/services/context/ContextListener.java
@@ -4,24 +4,16 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.springframework.web.context.ContextLoaderListener;
 import edu.utep.trustlab.toolkitOperators.util.*;
 public class ContextListener implements ServletContextListener {
 
-	private ContextLoaderListener springListener;
-	
-	public ContextListener(){
-		springListener = new ContextLoaderListener();
-	}
 	
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
-		springListener.contextDestroyed(event);
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		springListener.contextInitialized(event);
 		ServletContext context = event.getServletContext();
 		String serverURL = context.getInitParameter("server-url");
 		
