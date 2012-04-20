75aa1dad2a56f8ca7104ae48df742dfd80144f1d
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-19 23:50:04 -0600

    removed xmlschema attributes

--- toolkits/src/edu/utep/trustlab/services/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema (
-		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
-		attributeFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-
-package edu.utep.trustlab.services;
