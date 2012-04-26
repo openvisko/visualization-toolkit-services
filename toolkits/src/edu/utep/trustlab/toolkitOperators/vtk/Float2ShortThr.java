5f33d4f5c3162df2de893e8e569c797a60ca8616
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-04-25 23:06:37 -0600

    converted c program that converts from float array to short int array into java program.  did same for c program that converts int array to short int array

--- toolkits/src/edu/utep/trustlab/toolkitOperators/vtk/Float2ShortThr.java
+++ toolkits/src/edu/utep/trustlab/toolkitOperators/vtk/Float2ShortThr.java
@@ -1,18 +1,20 @@
 package edu.utep.trustlab.toolkitOperators.vtk;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 import edu.utep.trustlab.toolkitOperators.PassByReferenceOperator;
-import edu.utep.trustlab.toolkitOperators.util.CommandRunner;
 import edu.utep.trustlab.toolkitOperators.util.FileUtils;
 import edu.utep.trustlab.toolkitOperators.util.GetURLContents;
 public class Float2ShortThr extends PassByReferenceOperator{
 	
-	byte[] datasetOfFloats;
-	String randomString;
-	String inputDatasetFileName;
-	String inputDatasetFilePath;
-	String outputDatasetFileName;
-	String outputDatasetFilePath;
-	String outputDatasetURL;
+	private byte[] datasetOfFloats;
+	private String outputDatasetFileName;
+	private String outputDatasetFilePath;
+	private String outputDatasetURL;
 	
 	public Float2ShortThr(String datasetOfFloatsURL){
 		super(datasetOfFloatsURL);
@@ -20,21 +22,38 @@ public class Float2ShortThr extends PassByReferenceOperator{
 
 	protected void downloadInputs(String datasetOfFloatsURL){
 		datasetOfFloats = GetURLContents.downloadFile(datasetOfFloatsURL);
-		randomString = FileUtils.getRandomString();
-		inputDatasetFileName = "datasetOfFloats-" + randomString + ".f";
-		inputDatasetFilePath = FileUtils.writeBinaryFile(datasetOfFloats, FileUtils.getVTKWorkspace(), inputDatasetFileName);
 	}
 	
 	protected void setUpOutputs(){
-		outputDatasetFileName = "datasetOfInts-" + randomString + ".u";
+		outputDatasetFileName = "datasetOfInts-" + FileUtils.getRandomString() + ".u";
 		outputDatasetFilePath = FileUtils.makeFullPath(FileUtils.getVTKWorkspace(), outputDatasetFileName);
 		outputDatasetURL = FileUtils.getVTKOutputURLPrefix() + outputDatasetFileName;
 	}
 	
 	public String transform(String scalingFactor, String offset){
-	       String cmd = "float2ShortThr.sh " + inputDatasetFilePath + " " + outputDatasetFilePath + " " + scalingFactor + " " + offset;
-	       CommandRunner.run(cmd);
-	       
-	       return outputDatasetURL;
+		
+		float factor = Float.valueOf(scalingFactor);
+		float bias = Float.valueOf(offset);
+		
+		ByteBuffer byteBuffer = ByteBuffer.wrap(datasetOfFloats);
+		float[] floatArray = byteBuffer.asFloatBuffer().array();
+		
+		short[] shortArray = new short[floatArray.length];
+		for(int i = 0; i < floatArray.length; i ++){
+			shortArray[i] = (short)((floatArray[i] * factor) + bias);
+		}
+		
+		try{
+			FileOutputStream fileOutput = new FileOutputStream(new File(outputDatasetFilePath));
+			DataOutputStream dataOut = new DataOutputStream(fileOutput);
+			
+			for(short aShort : shortArray)
+				dataOut.write(aShort);
+			dataOut.close();
+			}catch(IOException e){
+				e.printStackTrace();
+		}
+		
+		return outputDatasetURL;
 	}
 }
--- toolkits/src/edu/utep/trustlab/toolkitOperators/vtk/Int2Short.java
+++ toolkits/src/edu/utep/trustlab/toolkitOperators/vtk/Int2Short.java
@@ -1,14 +1,17 @@
 package edu.utep.trustlab.toolkitOperators.vtk;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 import edu.utep.trustlab.toolkitOperators.PassByReferenceOperator;
-import edu.utep.trustlab.toolkitOperators.util.CommandRunner;
 import edu.utep.trustlab.toolkitOperators.util.FileUtils;
 import edu.utep.trustlab.toolkitOperators.util.GetURLContents;
 public class Int2Short extends PassByReferenceOperator{
 
 	byte[] datasetOfInts;
-	String inputDatasetFileName;
-	String inputDatasetFilePath;
 	String outputDatasetFileName;
 	String outputDatasetFilePath;
 	String outputDatasetURL;
@@ -19,19 +22,34 @@ public class Int2Short extends PassByReferenceOperator{
 
 	protected void downloadInputs(String datasetOfIntsURL){
 		datasetOfInts = GetURLContents.downloadFile(datasetOfIntsURL);
-		inputDatasetFileName = "datasetOfInts-"+ FileUtils.getRandomString() + ".i";
-		inputDatasetFilePath = FileUtils.writeBinaryFile(datasetOfInts, FileUtils.getVTKWorkspace(), inputDatasetFileName);
 	}
 	
 	protected void setUpOutputs(){
-		outputDatasetFileName = inputDatasetFileName + ".u";
+		outputDatasetFileName = "datasetOfInts-" + FileUtils.getRandomString() + ".u";
 		outputDatasetFilePath = FileUtils.makeFullPath(FileUtils.getVTKWorkspace(), outputDatasetFileName);
 		outputDatasetURL = FileUtils.getVTKOutputURLPrefix() + outputDatasetFileName;
 	}
 	
 	public String transform(){
-	       String cmd = "int2Short.sh " + inputDatasetFilePath + " " + outputDatasetFilePath;
-	       CommandRunner.run(cmd);
-	       return outputDatasetURL;
+		ByteBuffer byteBuffer = ByteBuffer.wrap(datasetOfInts);
+		int[] intArray = byteBuffer.asIntBuffer().array();
+		
+		short[] shortArray = new short[intArray.length];
+		for(int i = 0; i < intArray.length; i ++){
+			shortArray[i] = (short)intArray[i];
+		}
+		
+		try{
+			FileOutputStream fileOutput = new FileOutputStream(new File(outputDatasetFilePath));
+			DataOutputStream dataOut = new DataOutputStream(fileOutput);
+			
+			for(short aShort : shortArray)
+				dataOut.write(aShort);
+			dataOut.close();
+			}catch(IOException e){
+				e.printStackTrace();
+		}
+		
+		return outputDatasetURL;
 	}
 }
