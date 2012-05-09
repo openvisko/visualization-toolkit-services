fa77f89b452b1b96ee9159f565253ca8c49e1fd2
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-05-09 13:43:39 -0600

    added two kinds of scripts, one that call the commands using sudo and one that calls commands directly

--- toolkits/WebContent/scripts/prepare-for-unix.sh
+++ toolkits/WebContent/scripts/prepare-for-unix.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # change directory permissions so we can run dos2unix (writes files)
-sudo chmod 777 scripts-gmt/
-sudo chmod 777 scripts-ncl/
+chmod 777 scripts-gmt/
+chmod 777 scripts-ncl/
 
 dos2unix scripts-gmt/*.sh
 dos2unix scripts-ncl/*.sh
 
-sudo chmod 555 scripts-gmt/*.sh
-sudo chmod 555 scripts-ncl/*.sh
+chmod 555 scripts-gmt/*.sh
+chmod 555 scripts-ncl/*.sh
 
-sudo chmod 775 scripts-gmt/
-sudo chmod 775 scripts-ncl/
+chmod 775 scripts-gmt/
+chmod 775 scripts-ncl/
