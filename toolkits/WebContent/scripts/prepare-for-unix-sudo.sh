be48e3f745d1efa736e1c5607ee2942f0c4d0163
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-10-12 12:57:22 -0600

    added image magick to scripts path

--- toolkits/WebContent/scripts/prepare-for-unix-sudo.sh
+++ toolkits/WebContent/scripts/prepare-for-unix-sudo.sh
@@ -3,12 +3,16 @@
 # change directory permissions so we can run dos2unix (writes files)
 sudo chmod 777 scripts-gmt/
 sudo chmod 777 scripts-ncl/
+sudo chmod 777 scripts-imageMagick/
 
-dos2unix scripts-gmt/*.sh
-dos2unix scripts-ncl/*.sh
+dos2unix scripts-gmt/*
+dos2unix scripts-ncl/*
+dos2unix scripts-imageMagick/*
 
-sudo chmod 555 scripts-gmt/*.sh
-sudo chmod 555 scripts-ncl/*.sh
+sudo chmod 555 scripts-gmt/*
+sudo chmod 555 scripts-ncl/*
+sudo chmod 555 scripts-imageMagick/*
 
 sudo chmod 775 scripts-gmt/
 sudo chmod 775 scripts-ncl/
+sudo chmod 775 scripts-imageMagick/
--- toolkits/WebContent/scripts/prepare-for-unix.sh
+++ toolkits/WebContent/scripts/prepare-for-unix.sh
@@ -3,12 +3,16 @@
 # change directory permissions so we can run dos2unix (writes files)
 chmod 777 scripts-gmt/
 chmod 777 scripts-ncl/
+chmod 777 scripts-imageMagick/
 
-dos2unix scripts-gmt/*.sh
-dos2unix scripts-ncl/*.sh
+dos2unix scripts-gmt/*
+dos2unix scripts-ncl/*
+dos2unix scripts-imageMagick/*
 
-chmod 555 scripts-gmt/*.sh
-chmod 555 scripts-ncl/*.sh
+chmod 555 scripts-gmt/*
+chmod 555 scripts-ncl/*
+chmod 555 scripts-imageMagick/*
 
 chmod 775 scripts-gmt/
 chmod 775 scripts-ncl/
+chmod 775 scripts-imageMagick/
