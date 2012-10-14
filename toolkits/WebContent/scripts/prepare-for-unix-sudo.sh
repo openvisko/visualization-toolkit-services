a125090a8237d3e14876d471554178234d8f8564
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-10-14 12:36:40 -0600

    added .sh suffix

--- toolkits/WebContent/scripts/prepare-for-unix-sudo.sh
+++ toolkits/WebContent/scripts/prepare-for-unix-sudo.sh
@@ -5,13 +5,13 @@ sudo chmod 777 scripts-gmt/
 sudo chmod 777 scripts-ncl/
 sudo chmod 777 scripts-imageMagick/
 
-dos2unix scripts-gmt/*
-dos2unix scripts-ncl/*
-dos2unix scripts-imageMagick/*
+dos2unix scripts-gmt/*.sh
+dos2unix scripts-ncl/*.sh
+dos2unix scripts-imageMagick/*.sh
 
-sudo chmod 555 scripts-gmt/*
-sudo chmod 555 scripts-ncl/*
-sudo chmod 555 scripts-imageMagick/*
+sudo chmod 555 scripts-gmt/*.sh
+sudo chmod 555 scripts-ncl/*.sh
+sudo chmod 555 scripts-imageMagick/*.sh
 
 sudo chmod 775 scripts-gmt/
 sudo chmod 775 scripts-ncl/
--- toolkits/WebContent/scripts/prepare-for-unix.sh
+++ toolkits/WebContent/scripts/prepare-for-unix.sh
@@ -5,13 +5,13 @@ chmod 777 scripts-gmt/
 chmod 777 scripts-ncl/
 chmod 777 scripts-imageMagick/
 
-dos2unix scripts-gmt/*
-dos2unix scripts-ncl/*
-dos2unix scripts-imageMagick/*
+dos2unix scripts-gmt/*.sh
+dos2unix scripts-ncl/*.sh
+dos2unix scripts-imageMagick/*.sh
 
-chmod 555 scripts-gmt/*
-chmod 555 scripts-ncl/*
-chmod 555 scripts-imageMagick/*
+chmod 555 scripts-gmt/*.sh
+chmod 555 scripts-ncl/*.sh
+chmod 555 scripts-imageMagick/*.sh
 
 chmod 775 scripts-gmt/
 chmod 775 scripts-ncl/
