91975a43e1710493a5da3cd07e7f0c487d0588d1
Nicholas Del Rio <nicholas.delrio@gmail.com>
2012-10-24 18:19:05 -0600

    boring stuff

--- toolkits/src/edu/utep/trustlab/services/ToolkitServices.java
+++ toolkits/src/edu/utep/trustlab/services/ToolkitServices.java
@@ -2,6 +2,9 @@ package edu.utep.trustlab.services;
 
 import javax.jws.WebParam;
 
+import edu.utep.trustlab.toolkitOperators.cmmf.GenGeo;
+import edu.utep.trustlab.toolkitOperators.cmmf.H5WriteMesh;
+import edu.utep.trustlab.toolkitOperators.cmmf.PolyMesh_py;
 import edu.utep.trustlab.toolkitOperators.custom.CSVToTabularASCII;
 import edu.utep.trustlab.toolkitOperators.custom.Float2ShortThr;
 import edu.utep.trustlab.toolkitOperators.custom.GravityDataFieldFilter;
@@ -42,6 +45,33 @@ import edu.utep.trustlab.toolkitOperators.vtk.VTKVolume;
 import edu.utep.trustlab.toolkitOperators.vtk.VTKShepardMethod;
 
 public class ToolkitServices {
+	//cmmf services
+	public String GenGeo(
+			@WebParam(name="url") String url,
+			@WebParam(name="jobname") String jobname,
+			@WebParam(name="ngrains") String ngrains,
+			@WebParam(name="scale") String scale
+			){
+		GenGeo service = new GenGeo(url);
+		return service.transform(jobname, ngrains, scale);
+	}
+	
+	public String PolyMesh_py(
+			@WebParam(name="url") String url,
+			@WebParam(name="cel") String cel,
+			@WebParam(name="mid") String mid,
+			@WebParam(name="edge") String edge			
+			){
+		PolyMesh_py service = new PolyMesh_py(url);
+		return service.transform(cel, mid, edge);
+	}
+	
+	public String h5WriteMesh(@WebParam(name="url") String url)
+	{
+		H5WriteMesh service = new H5WriteMesh(url);
+		return service.transform();
+	}
+	
 	//imageJ services
 	public String surfacePlotter(
 			@WebParam(name="url") String url,
